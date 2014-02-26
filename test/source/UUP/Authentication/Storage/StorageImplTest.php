b1fe82674dcb44b0a42eb1571ee146d79c15299a
Anders Lövgren <andlov@nowise.se>
2014-02-26 16:50:43 +0000

    Use common base class for all storage backend tests.

---
+++ test/source/UUP/Authentication/Storage/StorageImplTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace UUP\Authentication\Storage;
+
+/**
+ * Runs unit test using object implementing the Storage interface.
+ *
+ * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+ */
+class StorageImplTest extends \PHPUnit_Framework_TestCase
+{
+
+        /**
+         * @var Storage 
+         */
+        protected $object;
+
+        /**
+         * @covers UUP\Authentication\Storage\FileStorage::exist
+         */
+        public function testExist()
+        {
+                $user = "olle";
+                $this->object->insert($user);
+                $this->assertEquals($this->object->exist($user), true);
+                $user = "adam";
+                $this->assertEquals($this->object->exist($user), false);
+        }
+
+        /**
+         * @covers UUP\Authentication\Storage\FileStorage::insert
+         */
+        public function testInsert()
+        {
+                $this->testExist();
+        }
+
+        /**
+         * @covers UUP\Authentication\Storage\FileStorage::remove
+         */
+        public function testRemove()
+        {
+                $user = "olle";
+                $this->object->insert($user);
+                $this->assertEquals($this->object->exist($user), true);
+                $this->object->remove($user);
+                $this->assertEquals($this->object->exist($user), false);
+        }
+        
+}
