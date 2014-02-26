21cef2dea1b4e79ab50d6a763f267129c6781aa8
Anders Lövgren <andlov@nowise.se>
2014-02-26 11:27:17 +0000

    Fixed unit test using composer class loader.

---
+++ test/bootstrap.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Copyright Error: on line 4, column 29 in Templates/Licenses/license-apache20.txt
+  The string doesn't match the expected date/time format. The string to parse was: "2014-feb-26". The expected format was: "MMM d, yyyy". Anders Lövgren (Computing Department at BMC, Uppsala University).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+ */
+require_once __DIR__ . '/../vendor/autoload.php';
+
+?>
