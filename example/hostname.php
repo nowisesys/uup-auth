d859529627a6a0174bab24dddd330d25d2f2892d
Anders Lövgren <andlov@nowise.se>
2014-02-28 03:05:36 +0000

    Added test driver for hostname authenticator.

---
+++ example/hostname.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+Copyright (C) 2014 Anders Lövgren (QNET/BMC CompDept).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<html>
+        <head>
+                <meta charset="UTF-8">
+                <title></title>
+        </head>
+        <body>
+                <?php
+                require_once __DIR__ . '/../vendor/autoload.php';
+
+                use UUP\Authentication\HostnameAuthenticator;
+
+                $hostname = new HostnameAuthenticator();
+                printf("<p>\n");
+                printf("Name: %s<br>\n", $hostname->getUser());
+                printf("Authenticated: %s<br>\n", $hostname->authenticated() ? "yes" : "no");
+                printf("</p>\n");
+
+                ?>
+        </body>
+</html>
