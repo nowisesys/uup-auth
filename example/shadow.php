b8e0792c1ad4b979f127844c559703191199a5c4
Anders Lövgren <andlov@nowise.se>
2014-03-02 16:55:28 +0000

    Adding shadow password authenticator with test driver (example).

---
+++ example/shadow.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<!--
+Copyright (C) 2014 Anders Lövgren (QNET/BMC CompDept).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+<html>
+        <head>
+                <meta charset="UTF-8">
+                <title>Shadow Password Authentication Example</title>
+        </head>
+        <body>
+                <?php
+                // ==========================================================================
+                //  Shadow password file authentication.
+                //  
+                //  The shadow password file must be readable by the user account the web
+                //  server is running as.
+                // ==========================================================================
+
+                require_once __DIR__ . '/../vendor/autoload.php';
+
+use UUP\Authentication\Storage\SharedMemoryStorage;
+use UUP\Authentication\Validator\ShadowValidator;
+use UUP\Authentication\BasicHttpAuthenticator;
+
+try {
+                        $validator = new ShadowValidator();
+                        $storage = new SharedMemoryStorage();
+                        $authenticator = new BasicHttpAuthenticator($validator, $storage, "Shadow Password Authentication Example");
+                        $authenticator->message = "Logon cancelled by caller";
+//                        $authenticator->redirect = basename(__FILE__);
+
+                        if (isset($_GET['login'])) {
+                                $authenticator->login();
+                        }
+                        if (isset($_GET['logout'])) {
+                                $authenticator->logout();
+                        }
+
+                        if ($authenticator->authenticated()) {
+                                printf("<p>Logged on as %s | <a href=\"?logout\">Logout</a>\n", $authenticator->getUser());
+                        } else {
+                                printf("<p><a href=\"?login\">Login</a>\n");
+                        }
+
+                        printf("<p>Use a system user account for login.</p>\n");
+                } catch (\Exception $exception) {
+                        printf("Exception: %s", $exception);
+                }
+
+                ?>
+        </body>
+</html>
---
+++ source/UUP/Authentication/Validator/ShadowValidator.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * Copyright (C) 2014 Anders Lövgren (QNET/BMC CompDept).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace UUP\Authentication\Validator;
+
+use UUP\Authentication\Exception;
+
+/**
+ * User account validation against the shadow password file. This file has 
+ * to be readable by the user accoutn the web server is running under:
+ * 
+ * <code>
+ * # Grant access to web server user using POSIX Access Control Lists (ACLs) on Gentoo Linux:
+ * bash$> setfacl -m u:apache:r /etc/shadow
+ * </code>
+ *
+ * <b>Warning:</b>
+ * The web server account needs to have read access to i.e. /etc/shadow if doing 
+ * local system account authentication. This may turn your application into an 
+ * system security problem.
+ * 
+ * @property-write string $shadow Set the shadow password file to use.
+ * 
+ * @author Anders Lövgren (QNET/BMC CompDept)
+ * @package UUP
+ * @subpackage Authentication
+ */
+class ShadowValidator extends CredentialValidator
+{
+
+        const delim = ':';
+
+        private $shadow;
+
+        /**
+         * Constructor.
+         * @param string $file The path to an alternative shadow password file.
+         */
+        public function __construct($file = '/etc/shadow')
+        {
+                $this->shadow = $file;
+        }
+
+        public function __set($name, $value)
+        {
+                if ($name == 'shadow') {
+                        $this->shadow = (string) $value;
+                }
+        }
+
+        public function authenticate()
+        {
+                if (($pass = $this->password()) != null) {
+                        return crypt($this->pass, $pass) == $pass;
+                } else {
+                        return false;
+                }
+        }
+
+        private function password()
+        {
+                if (($handle = fopen($this->shadow, "r"))) {
+                        while (($line = fgets($handle))) {
+                                if (strstr($line, $this->user)) {
+                                        $parts = explode(self::delim, $line);
+                                        return $parts[1];
+                                }
+                        }
+                        fclose($handle);
+                        return null;
+                } else {
+                        throw new Exception(sprintf("Failed open %s", $this->shadow));
+                }
+        }
+
+}
