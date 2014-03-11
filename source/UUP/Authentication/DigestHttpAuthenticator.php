0890967eca2b841c70fb23d960c0fc163aa8e128
Anders Lövgren <andlov@nowise.se>
2014-03-11 02:16:56 +0000

    Fixed namespace separation for HTTP digest code.

--- source/UUP/Authentication/DigestHttpAuthenticator.php
+++ source/UUP/Authentication/DigestHttpAuthenticator.php
@@ -16,193 +16,204 @@
  * limitations under the License.
  */
 
-namespace UUP\Authentication;
+namespace UUP\Authentication\Library\Authenticator {
 
-use UUP\Authentication\Validator\DigestHttpValidator;
-use UUP\Authentication\Validator\PasswordProvider;
-
-/**
- * Represent an Digest HTTP message.
- * 
- * @property-read string $nonce Message digest part.
- * @property-read string $nc Message digest part.
- * @property-read string $cnonce Message digest part.
- * @property-read string $qop Message digest part.
- * @property-read string $username Message digest part.
- * @property-read string $uri Message digest part.
- * @property-read string $response Message digest part.
- * @property-read string $algorithm Message digest part.
- * @property-read string $method Request method.
- * @property-read string $raw The raw digest message.
- * 
- * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
- * @package UUP
- * @subpackage Authentication
- */
-class DigestHttpMessage
-{
-
-        private $digest;        // raw digest message
-        private $data;          // parsed digest message.
-        private $method;        // request method
-        private $required;
+        use UUP\Authentication\Validator\DigestHttpValidator;
 
         /**
-         * Constructor.
-         * @param string $message The digest message to parse.
-         * @param array $required Array of required message digest parts.
+         * Represent an Digest HTTP message.
+         * 
+         * @property-read string $nonce Message digest part.
+         * @property-read string $nc Message digest part.
+         * @property-read string $cnonce Message digest part.
+         * @property-read string $qop Message digest part.
+         * @property-read string $username Message digest part.
+         * @property-read string $uri Message digest part.
+         * @property-read string $response Message digest part.
+         * @property-read string $algorithm Message digest part.
+         * @property-read string $method Request method.
+         * @property-read string $raw The raw digest message.
+         * 
+         * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+         * @package UUP
+         * @subpackage Authentication
          */
-        public function __construct($message, $required)
+        class DigestHttpMessage
         {
-                $this->digest = $message;
-                $this->required = $required;
 
-                $this->data = array();
-                $this->method = $_SERVER['REQUEST_METHOD'];
+                private $digest;        // raw digest message
+                private $data;          // parsed digest message.
+                private $method;        // request method
+                private $required;
 
-                $this->parse();
-        }
+                /**
+                 * Constructor.
+                 * @param string $message The digest message to parse.
+                 * @param array $required Array of required message digest parts.
+                 */
+                public function __construct($message, $required)
+                {
+                        $this->digest = $message;
+                        $this->required = $required;
 
-        public function __get($name)
-        {
-                switch ($name) {
-                        case 'method':
-                                return $this->method;
-                        case 'raw':
-                                return $this->digest;
-                        default:
-                                if (isset($this->data[$name])) {
-                                        return $this->data[$name];
-                                } else {
-                                        return false;
-                                }
+                        $this->data = array();
+                        $this->method = $_SERVER['REQUEST_METHOD'];
+
+                        $this->parse();
                 }
-        }
 
-        private function parse()
-        {
-                if (isset($this->digest)) {
-                        $needed = $this->required;
-                        $pattern = '@(\w+)=(?:(?:")([^"]+)"|([^\s,$]+))@';
-                        $matches = array();
-
-                        preg_match_all($pattern, $this->digest, $matches, PREG_SET_ORDER);
-
-                        foreach ($matches as $m) {
-                                $this->data[$m[1]] = $m[2] ? $m[2] : $m[3];
-                                if (isset($needed[$m[1]])) {
-                                        unset($needed[$m[1]]);
-                                }
-                        }
-                        if (!isset($needed)) {
-                                $this->data = array();
+                public function __get($name)
+                {
+                        switch ($name) {
+                                case 'method':
+                                        return $this->method;
+                                case 'raw':
+                                        return $this->digest;
+                                default:
+                                        if (isset($this->data[$name])) {
+                                                return $this->data[$name];
+                                        } else {
+                                                return false;
+                                        }
                         }
                 }
+
+                private function parse()
+                {
+                        if (isset($this->digest)) {
+                                $needed = $this->required;
+                                $pattern = '@(\w+)=(?:(?:")([^"]+)"|([^\s,$]+))@';
+                                $matches = array();
+
+                                preg_match_all($pattern, $this->digest, $matches, PREG_SET_ORDER);
+
+                                foreach ($matches as $m) {
+                                        $this->data[$m[1]] = $m[2] ? $m[2] : $m[3];
+                                        if (isset($needed[$m[1]])) {
+                                                unset($needed[$m[1]]);
+                                        }
+                                }
+                                if (!isset($needed)) {
+                                        $this->data = array();
+                                }
+                        }
+                }
+
+        }
+
+        /**
+         * Helper class for validation of Digest HTTP messages.
+         * 
+         * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+         * @package UUP
+         * @subpackage Authentication
+         */
+        class DigestHttpResponse
+        {
+
+                private $provider;
+
+                /**
+                 * Constructor
+                 * @param PasswordProvider $provider The password provider.
+                 */
+                public function __construct($provider)
+                {
+                        $this->provider = $provider;
+                }
+
+                /**
+                 * Validate the digest message.
+                 * @param DigestHttpMessage $message The digest message object.
+                 * @return bool True if digest match.
+                 */
+                public function validate($message, $realm)
+                {
+                        $user = $message->username;
+                        $pass = $this->provider->getPassword($user);
+                        $validator = new DigestHttpValidator($realm, $message, $user, $pass);
+                        return $validator->authenticate();
+                }
+
         }
 
 }
 
-/**
- * Helper class for validation of Digest HTTP messages.
- * 
- * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
- * @package UUP
- * @subpackage Authentication
- */
-class DigestHttpResponse
-{
+namespace UUP\Authentication {
 
-        private $provider;
+        use UUP\Authentication\Library\Authenticator\AuthenticatorBase,
+            UUP\Authentication\Validator\PasswordProvider;
 
         /**
-         * Constructor
-         * @param PasswordProvider $provider The password provider.
+         * Digest HTTP (WWW-Authenticate) authenticator.
+         *
+         * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+         * @package UUP
+         * @subpackage Authentication
          */
-        public function __construct($provider)
+        class DigestHttpAuthenticator extends AuthenticatorBase
         {
-                $this->provider = $provider;
-        }
 
-        /**
-         * Validate the digest message.
-         * @param DigestHttpMessage $message The digest message object.
-         * @return bool True if digest match.
-         */
-        public function validate($message, $realm)
-        {
-                $user = $message->username;
-                $pass = $this->provider->getPassword($user);
-                $validator = new DigestHttpValidator($realm, $message, $user, $pass);
-                return $validator->authenticate();
-        }
+                use Library\Authenticator\HttpAuthenticator;
 
-}
+                private $nonce;
 
-/**
- * Digest HTTP (WWW-Authenticate) authenticator.
- *
- * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
- * @package UUP
- * @subpackage Authentication
- */
-class DigestHttpAuthenticator implements Authenticator
-{
-        use HttpAuthenticator;
+                /**
+                 * Constructor.
+                 * 
+                 * The supplied Validator object must implement the PasswordProvider interface.
+                 * @param Validator $validator The validator callback object.
+                 * @param Storage $storage The storage backend object.
+                 * @param string $realm The authentication realm.
+                 * @param array $required Restriction on required digest parts.
+                 * @see PasswordProvider
+                 */
+                public function __construct($validator, $storage, $realm, $required = array('nonce' => 1, 'nc' => 1, 'cnonce' => 1, 'qop' => 1, 'username' => 1, 'uri' => 1, 'response' => 1, 'algorithm' => 0))
+                {
+                        $this->config($validator, $storage, $realm);
+                        $this->initialize($required);
+                }
 
-        private $nonce;
+                private function initialize($required)
+                {
+                        $this->nonce = uniqid();
 
-        /**
-         * Constructor. The supplied Validator object must implement the PasswordProvider
-         * interface.
-         * @param Validator $validator The validator callback object.
-         * @param Storage $storage The storage backend object.
-         * @param string $realm The authentication realm.
-         * @param array $required Restriction on required digest parts.
-         */
-        public function __construct($validator, $storage, $realm, $required = array('nonce' => 1, 'nc' => 1, 'cnonce' => 1, 'qop' => 1, 'username' => 1, 'uri' => 1, 'response' => 1, 'algorithm' => 0))
-        {
-                $this->config($validator, $storage, $realm);
-                $this->initialize($required);
-        }
-
-        private function initialize($required)
-        {
-                $this->nonce = uniqid();
-
-                if (isset($_SERVER['PHP_AUTH_DIGEST'])) {               // mod_php
-                        $digest = $_SERVER['PHP_AUTH_DIGEST'];
-                } elseif (isset($_SERVER['HTTP_AUTHENTICATION'])) {     // most other servers
-                        if (strpos(strtolower($_SERVER['HTTP_AUTHENTICATION']), 'digest') === 0) {
-                                $digest = substr($_SERVER['HTTP_AUTHORIZATION'], 7);
+                        if (isset($_SERVER['PHP_AUTH_DIGEST'])) {               // mod_php
+                                $digest = $_SERVER['PHP_AUTH_DIGEST'];
+                        } elseif (isset($_SERVER['HTTP_AUTHENTICATION'])) {     // most other servers
+                                if (strpos(strtolower($_SERVER['HTTP_AUTHENTICATION']), 'digest') === 0) {
+                                        $digest = substr($_SERVER['HTTP_AUTHORIZATION'], 7);
+                                }
+                        }
+                        if (isset($digest)) {
+                                // 
+                                // Kind of messy. Authentication is done in two steps:
+                                // 
+                                // 1. Authenticate the digest message.
+                                // 2. Authenticate the user/pass against the selected validator.
+                                // 
+                                $message = new DigestHttpMessage($digest, $required);
+                                $response = new DigestHttpResponse($this->validator);
+                                if ($response->validate($message, $this->realm)) {
+                                        $user = $message->username;
+                                        $pass = $this->validator->getPassword($user);
+                                        $this->validator->setCredentials($user, $pass);
+                                        $this->user = $message->username;
+                                }
                         }
                 }
-                if (isset($digest)) {
-                        // 
-                        // Kind of messy. Authentication is done in two steps:
-                        // 
-                        // 1. Authenticate the digest message.
-                        // 2. Authenticate the user/pass against the selected validator.
-                        // 
-                        $message = new DigestHttpMessage($digest, $required);
-                        $response = new DigestHttpResponse($this->validator);
-                        if ($response->validate($message, $this->realm)) {
-                                $user = $message->username;
-                                $pass = $this->validator->getPassword($user);
-                                $this->validator->setCredentials($user, $pass);
-                                $this->user = $message->username;
+
+                private function unauthorized()
+                {
+                        header(sprintf('WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"', $this->realm, $this->nonce, md5($this->realm)));
+                        header('HTTP/1.0 401 Unauthorized');
+                        if (isset($this->redirect)) {
+                                header(sprintf("Location: %s", $this->redirect));
+                        } else {
+                                die($this->message);
                         }
                 }
-        }
 
-        private function unauthorized()
-        {
-                header(sprintf('WWW-Authenticate: Digest realm="%s",qop="auth",nonce="%s",opaque="%s"', $this->realm, $this->nonce, md5($this->realm)));
-                header('HTTP/1.0 401 Unauthorized');
-                if (isset($this->redirect)) {
-                        header(sprintf("Location: %s", $this->redirect));
-                } else {
-                        die($this->message);
-                }
         }
 
 }
