c2ff0f498c3894122cc38c060b717fc9d86bbe00
Anders Lövgren <andlov@nowise.se>
2014-03-12 23:12:12 +0000

    Only call insert() on AuthenticatorChain object. Try to dynamic create object property, throw exception if failed.

--- source/UUP/Authentication/Stack/Access/ChainAccessBase.php
+++ source/UUP/Authentication/Stack/Access/ChainAccessBase.php
@@ -18,6 +18,9 @@
 
 namespace UUP\Authentication\Stack\Access;
 
+use UUP\Authentication\Stack\AuthenticatorChain,
+    UUP\Authentication\Exception;
+
 /**
  * Base class for chain access implementations.
  *
@@ -49,27 +52,65 @@ class ChainAccessBase
                 }
         }
 
+        /**
+         * Check if $name exist in this chain.
+         * @param string $name
+         * @return bool
+         */
         protected function exist($name)
         {
                 return $this->chain->exist($name);
         }
 
+        /**
+         * Get authenticator or chain matching $name.
+         * @param string $name
+         * @return Authenticator|AuthenticatorChain
+         */
         protected function get($name)
         {
                 return $this->chain->want($name);
         }
 
+        /**
+         * Set property or call member method or create sub chain.
+         * 
+         * This function performs three different tasks depending on whether this chain
+         * is an chain or an object:
+         * <ol>
+         * <li>Set object property $name to $value.</li>
+         * <li>Call member method $name with $value.</li>
+         * <li>Insert object $value in this chain.</li>
+         * </ol>
+         * The last case (3) requires that this chain is an instance of AuthenticatorChain.
+         * This function is kind of relaxed, its perfectly fine that the inserted value is
+         * not an object at all.
+         * 
+         * This function throws an exception if the this chain refers to an immutable 
+         * object that can't be modified (setting property in object fails).
+         * 
+         * @param string $name
+         * @param mixed $value
+         * @throws Exception
+         */
         protected function set($name, $value)
         {
                 if (property_exists($this->chain, $name)) {
-                        $this->chain->$name = $value; // $chain[$offset] = $value;
+                        $this->chain->$name = $value;           // set object property
                 } elseif (method_exists($this->chain, $name)) {
-                        $this->chain->$name($value);  // call method using array subscript
-                } else {
-                        $this->chain->insert($name, $value);
+                        $this->chain->$name($value);            // call member method
+                } elseif ($this->chain instanceof AuthenticatorChain) {
+                        $this->chain->insert($name, $value);    // insert in chain (relaxed)
+                } elseif (($this->chain->$name = $value) && ($this->chain->$name !== $value)) {
+                        // Logic error that is hard to detect and will cause data loss.
+                        throw new Exception(sprintf('Failed set property %s on immutable non-chain object (%s)', $name, get_class($this->chain)));
                 }
         }
 
+        /**
+         * Remove object matching $name from this chain.
+         * @param string $name
+         */
         protected function remove($name)
         {
                 $this->chain->remove($name);
